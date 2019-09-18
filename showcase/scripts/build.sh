d7227c61293b8212197d4007d84b9d2b420fc8cd
Davide <david3bizzarri@gmail.com>
2019-09-18 09:08:00 +0200

    feat: add steps to build ios app

--- showcase/scripts/build.sh
+++ showcase/scripts/build.sh
@@ -7,43 +7,80 @@ IONIC_SHOWCASE="https://github.com/aerogear/ionic-showcase.git"
 MOBILE_PLATFORM="${1:-$MOBILE_PLATFORM}"
 
 APPS_DIR="$PWD/apps"
-APP_SRC=
-APP_DST=
+APP_NAME="ionic-showcase"
+
+function buildIOS() {
+
+    # prepare ios using ionic and cordova
+    ionic cordova platform add ios
+    ionic cordova prepare ios
+
+    # build ios using xcode without certificates
+    cd platforms/ios
+
+    # build using xcode
+    xcodebuild \
+        -workspace "AeroGear Ionic Showcase.xcworkspace" \
+        -scheme "AeroGear Ionic Showcase" \
+        -configuration Debug \
+        -derivedDataPath ./derived \
+        CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+
+    # create the .ipa file
+    mkdir -p package/Payload
+    cp -r derived/Build/Products/Debug-iphoneos/AeroGear\ Ionic\ Showcase.app package/Payload/
+    cd package/
+    zip -r AeroGear\ Ionic\ Showcase.ipa Payload/
+
+    # copy the app to the apps dir
+    cp -f AeroGear\ Ionic\ Showcase.ipa ${APPS_DIR}/${APP_NAME}.ipa
+
+    echo "+ built \"${APPS_DIR}/${APP_NAME}.ipa\""
+}
+
+function buildAndroid() {
+
+    # build android using ionic and cordova
+    ionic cordova build android --device --debug
+
+    # copy the app to the apps dir
+    cp -f platforms/android/app/build/outputs/apk/debug/app-debug.apk ${APPS_DIR}/${APP_NAME}.apk
+
+    echo "+ built \"${APPS_DIR}/${APP_NAME}.apk\""
+}
+
+if [[ -z "${MOBILE_PLATFORM}" ]]; then
+    echo "error: MOBILE_PLATFORM is not defined"
+    exit 1
+fi
+
+mkdir -p ${APPS_DIR}
+
+WORKSPACE="$(mktemp -d)"
+
+echo "+ cloning ionic showcase in \"${WORKSPACE}\""
+
+git clone ${IONIC_SHOWCASE} ${WORKSPACE}
+
+echo "+ build ionic showcase"
+
+cd ${WORKSPACE}
+
+npm install
 
 case ${MOBILE_PLATFORM} in
 android)
-    APP_SRC="platforms/android/app/build/outputs/apk/debug/app-debug.apk"
-    APP_DST="$APPS_DIR/ionic-showcase.apk"
+    buildAndroid
     ;;
 ios)
-    APP_SRC="platforms/ios/build/device/HelloCordova.ipa"
-    APP_DST="$APPS_DIR/ionic-showcase.ipa"
+    buildIOS
     ;;
 *)
-    echo "error: MOBILE_PLATFORM is not defined or not valid"
+    echo "error: \"${MOBILE_PLATFORM}\" is not a valid MOBILE_PLATFORM"
     exit 1
     ;;
 esac
 
-TMPDIR="$(mktemp -d)"
+echo "+ clean \"${WORKSPACE}\""
 
-echo "+ cloning ionic showcase in \"${TMPDIR}\""
-
-git clone ${IONIC_SHOWCASE} ${TMPDIR}
-
-cd ${TMPDIR}/
-
-echo "+ build ionic showcase"
-
-npm install
-
-ionic cordova build ${MOBILE_PLATFORM}
-
-echo "+ copy app to apps/"
-
-mkdir -p ${APPS_DIR}
-cp -f ${APP_SRC} ${APP_DST}
-
-echo "+ clean \"${TMPDIR}\""
-
-rm -rf ${TMPDIR}/
+rm -rf ${WORKSPACE}
