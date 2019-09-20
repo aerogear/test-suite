07387f8fec1f607791946589ceaa31fb566acc21
Pavel Šturc <psturc@redhat.com>
2019-09-20 11:16:16 +0200

    fix: page as global var, no default viewport

--- browser/utils/bootstrap.js
+++ browser/utils/bootstrap.js
@@ -7,12 +7,14 @@ const opts = {
     // Uncommenting the next line will slow down all actions
     // performed by the Puppeteer by <value> milliseconds.
     // Pretty useful when fixing/writing a test
-    // slowMo: 50
+    slowMo: 10,
+    defaultViewport: null
 }
 
 before(async () => {
     global.expect = expect
     global.browser = await puppeteer.launch(opts)
+    global.page
 })
 
 after(() => {
