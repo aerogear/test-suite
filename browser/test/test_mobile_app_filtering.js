a355f46d4defb8d02598b7771f73ad6e305c81b8
Stephin Thomas <stethoma@redhat.com>
2019-10-02 14:31:04 +0200

    Chore:Added unique names for app

--- browser/test/test_mobile_app_filtering.js
+++ browser/test/test_mobile_app_filtering.js
@@ -3,10 +3,17 @@ const {
     createApp,
     deleteApp
 } = require('../utils/mdc')
-const appName1 = "test1",
-    appName2 = "test2",
-    appName3 = "verification",
-    appName4 = "testing",
+    // const appName1 = `test1-${Date.now()}`,
+    //     appName2 = `test2-${Date.now()}`,
+    //     appName3 = `verification-${Date.now()}`,
+    //     appName4 = `testing-${Date.now()}`,
+    //     filter1 = "test",
+    //     filter2 = "verif",
+    //     filter3 = "testin"
+const appName1 = `test-${Math.ceil(Math.random()*1000)}`,
+    appName2 = `test-${Math.ceil(Math.random()*1000)}`,
+    appName3 = `verification-${Math.ceil(Math.random()*1000)}`,
+    appName4 = `testing-${Math.ceil(Math.random()*1000)}`,
     filter1 = "test",
     filter2 = "verif",
     filter3 = "testin"
@@ -30,31 +37,31 @@ describe('Testing MDC Apps filtering', () => {
         await page.$eval('.toolbar-pf-filter input.toolbarFilter', el => el.value = "")
     })
 
-    it("MDC overview page should display only Test* apps", async() => {
+    it("MDC overview page should display only test* apps", async() => {
         await page.waitForSelector('.toolbar-pf-filter input.toolbarFilter')
         await page.type('input.toolbarFilter', "test")
         await page.waitForFunction(
-            filter1 => [...document.querySelectorAll('.card-pf-title > h1')].every(el => (el.innerText).slice(0, 4) === filter1), {},
+            filter1 => [...document.querySelectorAll('.card-pf-title > h1')].every(el => (el.innerText).includes(filter1)), {},
             filter1
         )
     })
-    it("MDC Overview page should display only Verification App", async() => {
+    it("MDC Overview page should display only Verification* App", async() => {
         await page.waitForSelector('.toolbar-pf-filter input.toolbarFilter')
         await page.type('input.toolbarFilter', "verif", { delay: 100 })
         await page.waitForFunction(
-            filter2 => [...document.querySelectorAll('.card-pf-title > h1')].every(el => (el.innerText).slice(0, 5) === filter2), {},
+            filter2 => [...document.querySelectorAll('.card-pf-title > h1')].every(el => (el.innerText).includes(filter2)), {},
             filter2
         )
     })
-    it("Delete test app and add testing app", async() => {
+    it("Delete test* app and add testing* app", async() => {
         await deleteApp(appName1)
         await createApp(appName4)
     })
-    it("MDC Overview page should display only testing App", async() => {
+    it("MDC Overview page should display only testing* App", async() => {
         await page.waitForSelector('.toolbar-pf-filter input.toolbarFilter')
         await page.type('input.toolbarFilter', "testin", { delay: 100 })
         await page.waitForFunction(
-            filter3 => [...document.querySelectorAll('.card-pf-title > h1')].every(el => (el.innerText).slice(0, 6) === filter3), {},
+            filter3 => [...document.querySelectorAll('.card-pf-title > h1')].every(el => (el.innerText).includes(filter3)), {},
             filter3
         )
     })
