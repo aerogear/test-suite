dd294741cfb69f6eae1da4d1009f9ab40bab8405
Davide <david3bizzarri@gmail.com>
2019-10-15 16:49:04 +0200

    refactor: remove the ids

--- showcase/test/security.ts
+++ showcase/test/security.ts
@@ -5,35 +5,48 @@ import { expect } from "chai";
 describe("Security", function() {
   it("should display number of passed checks", async () => {
     // Open Menu
-    retry(async () => {
+    await retry(async () => {
       const e = await device.$("ion-menu-button");
       await interact(e, e => shadowClick(e, "button"));
-      await (await $("ion-menu")).waitForDisplayed();
+      await (await device.$("ion-menu")).waitForDisplayed();
     });
 
     // Find security tab
-    retry(async () => {
-      const e = await device.$("#e2e-menu-item-security");
+    await retry(async () => {
+      const e = await device.$(() => {
+        return Array.from(document.querySelectorAll("ion-menu ion-item")).find(
+          e => {
+            const l = e.querySelector("ion-label");
+            return l !== null && l.textContent === "Security";
+          }
+        );
+      });
       await interact(e, e => e.click());
-      await (await device.$("#e2e-menu-item-devicetrust")).waitForDisplayed();
+      await (await device.$(
+        'ion-menu ion-item[routerLink="/devicetrust"]'
+      )).waitForDisplayed();
     });
 
     // Go to Device Trust
-    retry(async () => {
-      const e = await device.$("#e2e-menu-item-devicetrust");
+    await retry(async () => {
+      const e = await device.$('ion-menu ion-item[routerLink="/devicetrust"]');
       await interact(e, e => e.click());
       await e.waitForDisplayed(undefined, true);
     });
 
     // Device Trust
-    retry(async () => {
+    await retry(async () => {
       const e = await device.$("devicetrust");
       await e.waitForDisplayed();
     });
 
     // Wait for the checks to pass
-    retry(async () => {
-      const e = await device.$("#e2e-devicetrust-passed");
+    await retry(async () => {
+      const e = await device.$(() => {
+        return document
+          .querySelectorAll("devicetrust ion-grid ion-row")[2]
+          .querySelector("div");
+      });
       expect(await e.getText()).match(/\(\d+ out of \d+ checks passing\)/);
     }, 5000);
   });
