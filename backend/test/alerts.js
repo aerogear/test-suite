68773710b3fcd8733e6a3b4230378d9dead864e3
Stephin Thomas <stethoma@redhat.com>
2019-10-15 16:47:37 +0200

    Chore: get oauth token from common folder

--- backend/test/alerts.js
+++ backend/test/alerts.js
@@ -1,47 +1,18 @@
-const util = require('util')
+// @ts-nocheck
+const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const axios = require('axios');
-const { waitFor } = require('../../common/util/utils');
-const puppeteer = require('puppeteer')
+const { waitFor, getOAuthProxy } = require('../../common/util/utils');
+const puppeteer = require('puppeteer');
 const { init, getMdcNamespace } = require('../../common/util/rhmds-api');
-
 describe('Alerts test', function() {
     this.timeout(0);
     let headers;
     let browser;
-    before("authenticate with oauth proxy", async() => {
-        const openshiftUser = process.env.OPENSHIFT_USER;
-        const openshiftPass = process.env.OPENSHIFT_PASS;
-        if (openshiftUser === undefined || openshiftPass === undefined) {
-            throw new Error(
-                "OPENSHIFT_USER and/or OPENSHIFT_PASS are not defined"
-            );
-        }
-        browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(process.env['ALERT_URL']);
-        await Promise.all([page.waitForNavigation(), page.click("button")]);
-        await page.type("#username", openshiftUser);
-        await page.type("#password", openshiftPass);
-        await Promise.all([
-            page.waitForNavigation(),
-            page.click("#kc-login"),
-        ]);
-        const approveButton = await page.$('input[name="approve"]');
-        if (approveButton) {
-            await Promise.all([
-                page.waitForNavigation(),
-                approveButton.click(),
-            ]);
-        }
-        const cookies = await page.cookies();
-        const cookie = cookies.find(c => c.name === "_oauth_proxy").value;
-        headers = { Cookie: `_oauth_proxy=${cookie}` };
+    before('authenticate with oauth proxy', async() => {
+        headers = await getOAuthProxy(process.env['ALERT_URL']);
     });
 
-    after("close browser", async() => {
-        await browser.close();
-    });
     it('should trigger mdc down alert', async() => {
         await init();
         const mdcNamespace = getMdcNamespace();
@@ -52,10 +23,12 @@ describe('Alerts test', function() {
                 headers,
                 method: 'get'
             });
-            return result.data.data.alerts.find(alert => alert.labels.alertname === 'MobileDeveloperConsoleContainerDown');
+            return result.data.data.alerts.find(
+                alert =>
+                alert.labels.alertname === 'MobileDeveloperConsoleContainerDown'
+            );
         }, 5 * 60000);
 
         await exec(`oc scale --replicas=1 dc mdc -n ${mdcNamespace}`);
-
     });
 });
--- common/util/utils.js
+++ common/util/utils.js
@@ -1,3 +1,8 @@
+const puppeteer = require('puppeteer');
+let browser;
+let headers;
+const openshiftUser = process.env.OPENSHIFT_USER;
+const openshiftPass = process.env.OPENSHIFT_PASS;
 const waitFor = async (check, timeout, pause = 4000) => {
   return new Promise(async (resolve, reject) => {
     let timedout = false;
@@ -19,8 +24,42 @@ const waitFor = async (check, timeout, pause = 4000) => {
 };
 
 const randomString = () => Math.random().toString(36).substring(7);
+const getOAuthProxy = async serviceURL => {
+    if (openshiftUser === undefined || openshiftPass === undefined) {
+        throw new Error('OPENSHIFT_USER and/or OPENSHIFT_PASS are not defined');
+    }
+
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(serviceURL);
+    await Promise.all([
+        page.waitForNavigation(), 
+        page.click('button')
+    ]);
+    await page.type('#username', openshiftUser);
+    await page.type('#password', openshiftPass);
+    await Promise.all([
+        page.waitForNavigation(), 
+        page.click('#kc-login')
+    ]);
+    const approveButton = await page.$('input[name="approve"]');
+    if (approveButton) {
+        await Promise.all([
+            page.waitForNavigation(), 
+            approveButton.click()
+        ]);
+    }
+
+    const cookies = await page.cookies();
+    const cookie = cookies.find(c => c.name === '_oauth_proxy').value;
+    headers = { Cookie: `_oauth_proxy=${cookie}` };
+    await browser.close();
+    return headers;
+};
 
 module.exports = {
   waitFor,
-  randomString
+  randomString,
+  getOAuthProxy
 };
+
