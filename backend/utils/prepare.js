bfad6f0e544129e0cbe3f7063374c75282f72c3d
Jan Hellar <jhellar@redhat.com>
2020-01-08 15:04:25 +0100

    prettier

--- backend/utils/prepare.js
+++ backend/utils/prepare.js
@@ -1,10 +1,4 @@
-const {
-  init,
-  redeployShowcase,
-  resource,
-  TYPE,
-  ACTION
-} = require("../../common/util/rhmds-api");
+const { init } = require("../../common/util/rhmds-api");
 
 const name = "backend-test-suite";
 process.env["APP_NAME"] = name;
@@ -12,8 +6,4 @@ process.env["APP_NAME"] = name;
 before("redeploy showcase server", async function() {
   this.timeout(0);
   await init();
-  // await resource(TYPE.MOBILE_APP, ACTION.DELETE, name).catch(() => {});
-  // const namespace = await redeployShowcase(name);
-  // const routes = await resource(TYPE.ROUTE, ACTION.GET_ALL, null, namespace);
-  // process.env["SYNC_URL"] = `https://${routes.items[0].spec.host}`;
 });
