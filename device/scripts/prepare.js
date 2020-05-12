999cec6f42709b27af65f5ae7847b6d94a1fc701
Martin Hesko <martinhesko@localhost.localdomain>
2020-05-12 11:14:00 +0200

    updated to grab token automatically

--- device/scripts/prepare.js
+++ device/scripts/prepare.js
@@ -3,14 +3,38 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 const fs = require("fs");
+const openshiftUser = process.env.OPENSHIFT_USERNAME;
+const openshiftPassword = process.env.OPENSHIFT_PASSWORD;
+const openshiftURL = process.env.OPENSHIFT_URL;
 
-const { redeployShowcase, init } = require("../../common/util/rhmds-api");
+const {
+  redeployShowcase,
+  init,
+  getNamespaces,
+  resource,
+  TYPE,
+  ACTION,
+  userLogin,
+} = require("../../common/util/rhmds-api");
+const { getOpenshiftAPItoken } = require("../../common/util/utils");
 
 const appName = "device-test-suite";
 
 (async () => {
   await init();
-
+  let authProject = await getNamespaces("openshift-authentication");
+  let authHostname = (
+    await resource(TYPE.ROUTE, ACTION.GET_ALL, null, authProject)
+  ).items
+    .map((r) => r.spec.host)
+    .find((url) => url.includes("oauth-openshift"));
+  let authEndpoint = `https://${authHostname}/oauth/token/request`;
+  let token = await getOpenshiftAPItoken(
+    authEndpoint,
+    openshiftUser,
+    openshiftPassword
+  );
+  await userLogin(openshiftURL, token.token);
   const project = await redeployShowcase(appName);
 
   const output = await exec(
